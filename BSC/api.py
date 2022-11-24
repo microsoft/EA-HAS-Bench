f702e53c34c393e6b8ea13f544b0249897b370d1
Shuguang Dou <33367425+shuguang-52@users.noreply.github.com>
2022-11-24 15:13:53 +0800

    Update api.py

--- BSC/api.py
+++ BSC/api.py
@@ -1,8 +1,5 @@
-"""
-This file contains code based on
-https://github.com/automl/nasbench301/
-Authors: Julien Siems, Lucas Zimmer, Arber Zela, Jovita Lukasik, Margret Keuper, Frank Hutter
-"""
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 
 import os
