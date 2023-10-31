b06e9f8af01dbcfa31d040f5d21e735710321cd6
Shuguang Dou <33367425+shuguang-52@users.noreply.github.com>
2023-10-31 14:47:57 +0800

    Update __init__.py

--- naslib/search_spaces/__init__.py
+++ naslib/search_spaces/__init__.py
@@ -1,5 +1,5 @@
 from .darts.graph import DartsSearchSpace
 from .nasbench101.graph import NasBench101SearchSpace
 from .nasbench201.graph import NasBench201SearchSpace, NasBench211SearchSpace
-from .nasbenchgreen.graph import NasBenchGreenSearchSpace
+from .eahasbench.graph import NasBenchGreenSearchSpace
 from .nasbenchnlp.graph import NasBenchNLPSearchSpace
